dd1efd0a065898f433a5f9c14081561ce2f17d2e
archit <tripathiarchit432@gmail.com>
2020-06-13 20:24:32 +0530

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
-        <p className="copyright">
-         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="http://www.adityaketkar.me"> Aditya Ketkar</a>  
+       <p className="copyright">
+         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="http://www.adityaketkar.me"> Archit Tripathi</a>
         <br/>
-        Get your own version of this webpage <a href="https://github.com/adityaketkar/circle-packing-personal-homepage"> here </a>
+        Get your own version of this webpage <a href="https://github.com/architviper/circle-packing-personal-homepage"> here </a>
         </p>
     </footer>
 )
